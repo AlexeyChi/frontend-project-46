8fcd920bd9a089c83025c35ec47565394f02699b
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-11-27 21:07:06 +0300

    Apdated func

--- src/index.js
+++ src/index.js
@@ -11,24 +11,44 @@ const genDiff = (filepath1, filepath2) => {
   const keys1 = Object.keys(parsedFile1).sort();
   const keys2 = Object.keys(parsedFile2).sort();
   const uniqKeys = [...new Set(keys1.concat(keys2))];
-  const result = uniqKeys.reduce((acc, key) => {
-    if (keys1.includes(key) && keys2.includes(key)) {
-      if (parsedFile1[key] !== parsedFile2[key]) {
-        acc += `  - ${key}: ${parsedFile1[key]}\n  + ${key}: ${parsedFile2[key]}\n`;
+  const result = uniqKeys.map((key) => {
+    let line;
+    if (Object.hasOwn(parsedFile1, key) && Object.hasOwn(parsedFile2, key)) {
+      if (parsedFile1[key] === parsedFile2[key]) {
+        line = { status: 'unchanged', key };
+      } else {
+        line = { status: 'changed', key };
       }
-      acc += `    ${key}: ${parsedFile1[key]}\n`;
     }
-    if (!keys2.includes(key)) {
-      acc += `  - ${key}: ${parsedFile1[key]}\n`;
+    if (!Object.hasOwn(parsedFile1, key)) {
+      line = { status: 'added', key };
     }
-    if (!keys1.includes(key)) {
-      acc += `  + ${key}: ${parsedFile2[key]}\n`;
+    if (!Object.hasOwn(parsedFile2, key)) {
+      line = { status: 'deleted', key };
     }
+    return line;
+  })
+    .map(({ status, key }) => {
+      switch (status) {
+        case 'added': {
+          return `  + ${key}: ${parsedFile2[key]}`;
+        }
+        case 'deleted': {
+          return `  - ${key}: ${parsedFile1[key]}`;
+        }
+        case 'unchanged': {
+          return `    ${key}: ${parsedFile1[key]}`;
+        }
+        case 'changed': {
+          return `  - ${key}: ${parsedFile1[key]}\n  + ${key}: ${parsedFile2[key]}`;
+        }
+        default:
+          throw new Error(`Unnamed ${status}`);
+      }
+    })
+    .join('\n');
 
-    return acc;
-  }, '');
-
-  return `{\n${result}}`;
+  return `{\n${result}\n}`;
 };
 
 export default genDiff;
