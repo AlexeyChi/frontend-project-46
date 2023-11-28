43329d3a81339d6acb3847b012cdc48c41f41f32
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-11-28 12:30:25 +0300

    Maked parser func

---
+++ src/parsers.js
@@ -0,0 +1,17 @@
+import yaml from 'js-yaml';
+
+const parser = (data, format) => {
+  switch (format) {
+    case 'json': {
+      return JSON.parse(data);
+    }
+    case 'yml':
+    case 'yaml': {
+      return yaml.load(data);
+    }
+    default:
+      throw new Error(`Unnamed ${format}`);
+  }
+};
+
+export default parser;
