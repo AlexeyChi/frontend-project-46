fca21bde60cc59f574355195532f543eef59c0fd
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-11-30 22:41:09 +0300

    Added buildTree.js, stylish.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import parser from './parsers.js';
-import compared from './compareData.js';
+import buildTree from './buildTree.js';
+import stylish from './formatters/stylish.js';
 
 const makeFullPath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(makeFullPath(filename), 'utf-8');
@@ -12,22 +13,9 @@ const genDiff = (filename1, filename2) => {
   const parsedFile1 = parser(data1, path.extname(filename1).slice(1));
   const parsedFile2 = parser(data2, path.extname(filename2).slice(1));
 
-  const getDifStatus = compared(parsedFile1, parsedFile2);
-  const result = getDifStatus.map(({
-    status, key, valueBefore, valueAfter,
-  }) => {
-    switch (status) {
-      case 'added': return `  + ${key}: ${valueAfter}`;
-      case 'deleted': return `  - ${key}: ${valueBefore}`;
-      case 'unchanged': return `    ${key}: ${valueBefore}`;
-      case 'changed': return `  - ${key}: ${valueBefore}\n  + ${key}: ${valueAfter}`;
-      default:
-        throw new Error(`Unnamed ${status}`);
-    }
-  })
-    .join('\n');
-
-  return `{\n${result}\n}\n`;
+  const associationTree = buildTree(parsedFile1, parsedFile2);
+  const result = stylish(associationTree);
+  return result;
 };
 
 export default genDiff;
