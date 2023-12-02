7e4b2f54302dd2a935013555b5805df6692de862
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-12-02 23:23:00 +0300

    Added processing of different formats

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,8 +11,8 @@ program
 program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, option) => {
+    console.log(genDiff(filepath1, filepath2, option.format));
   });
 
 program.parse();
