a829d2aa26e34f2699525051c026e44d9da9ab20
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-12-02 23:07:34 +0300

    Added plain format test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -9,12 +9,20 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const formats = ['json', 'yml'];
-const expected = readFile('etalonJSON.txt');
+const options = [
+  { file1: 'json', file2: 'json', format: 'stylish' },
+  { file1: 'json', file2: 'yml', format: 'stylish' },
+  { file1: 'yml', file2: 'yml', format: 'stylish' },
+  { file1: 'json', file2: 'json', format: 'plain' },
+  { file1: 'yml', file2: 'yml', format: 'plain' },
+  { file1: 'json', file2: 'yml', format: 'plain' },
+];
 
-test.each(formats)('testing formates', (format) => {
-  const data1 = `file1.${format}`;
-  const data2 = `file2.${format}`;
-  const result = genDiff(data1, data2);
+test.each(options)('testing formates', (option) => {
+  const data1 = `file1.${option.file1}`;
+  const data2 = `file2.${option.file2}`;
+  const etalonFile = `etalon${option.format.toUpperCase()}.txt`;
+  const expected = readFile(etalonFile);
+  const result = genDiff(data1, data2, option.format);
   expect(result).toEqual(expected);
 });
