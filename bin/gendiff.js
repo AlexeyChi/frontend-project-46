14daeec6f9afdec907cb8c8df46a8b8667236769
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-12-03 22:40:38 +0300

    Refactoring

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .helpOption('-h, --help', 'output usage information');
-
-program
+  .helpOption('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2, option) => {
