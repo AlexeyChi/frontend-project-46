503d64e9b8cbdcbbe8be6896153601a55eb611d3
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-12-03 23:42:05 +0300

    Added YAML format

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -9,29 +9,46 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+const stylishData = readFile('etalonSTYLISH.txt');
+const plainData = readFile('etalonPLAIN.txt');
+const jsonData = readFile('etalonJSON.txt');
+
 const options = [
-  { file1: 'json', file2: 'json', format: 'stylish' },
-  { file1: 'json', file2: 'yml', format: 'stylish' },
-  { file1: 'yml', file2: 'yml', format: 'stylish' },
-  { file1: 'json', file2: 'json', format: 'plain' },
-  { file1: 'yml', file2: 'yml', format: 'plain' },
-  { file1: 'json', file2: 'yml', format: 'plain' },
-  { file1: 'json', file2: 'json', format: 'json' },
-  { file1: 'yml', file2: 'yml', format: 'json' },
-  { file1: 'json', file2: 'yml', format: 'json' },
+  {
+    file1: 'file1.json', file2: 'file2.json', format: 'stylish', expected: stylishData,
+  },
+  {
+    file1: 'file1.yml', file2: 'file2.yml', format: 'stylish', expected: stylishData,
+  },
+  {
+    file1: 'file1.yaml', file2: 'file2.yaml', format: 'stylish', expected: stylishData,
+  },
+  {
+    file1: 'file1.json', file2: 'file2.json', format: 'plain', expected: plainData,
+  },
+  {
+    file1: 'file1.yml', file2: 'file2.yml', format: 'plain', expected: plainData,
+  },
+  {
+    file1: 'file1.yaml', file2: 'file2.yaml', format: 'plain', expected: plainData,
+  },
+  {
+    file1: 'file1.yml', file2: 'file2.json', format: 'json', expected: jsonData,
+  },
+  {
+    file1: 'file1.yml', file2: 'file2.yml', format: 'json', expected: jsonData,
+  },
+  {
+    file1: 'file1.yaml', file2: 'file2.yaml', format: 'json', expected: jsonData,
+  },
 ];
 
 const detaultFormat = 'stylish';
 
 test.each(options)('testing formates', (option) => {
-  const data1 = `file1.${option.file1}`;
-  const data2 = `file2.${option.file2}`;
-  const etalonFile = `etalon${option.format.toUpperCase()}.txt`;
-  const expected = readFile(etalonFile);
-  const result = genDiff(data1, data2, option.format);
-  expect(result).toEqual(expected);
+  const result = genDiff(option.file1, option.file2, option.format);
+  expect(result).toEqual(option.expected);
 
-  const detaultDiff = genDiff(data1, data2, detaultFormat);
-  const defaultResult = readFile('etalonSTYLISH.txt');
-  expect(detaultDiff).toEqual(defaultResult);
+  const detaultDiff = genDiff(option.file1, option.file2, detaultFormat);
+  expect(detaultDiff).toEqual(stylishData);
 });
