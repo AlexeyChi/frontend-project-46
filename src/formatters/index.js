14ce8fc56eb31c210a0e634611e93e0c7c39b41a
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-12-04 00:00:37 +0300

    Refactoring

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,6 +1,5 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
-import json from './json.js';
 
 const formatters = (data, format) => {
   switch (format) {
@@ -9,7 +8,7 @@ const formatters = (data, format) => {
     case 'plain':
       return plain(data);
     case 'json':
-      return json(data);
+      return JSON.stringify(data);
     default:
       throw new Error('Unknown format, please try again');
   }
