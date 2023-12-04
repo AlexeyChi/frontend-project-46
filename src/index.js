d500fde808165582ea90cfd0ff2db0de50af0a82
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-12-04 23:34:19 +0300

    Refreshed

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import parser from './parsers.js';
 import buildTree from './buildTree.js';
 import formatters from './formatters/index.js';
 
-const makeFullPath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(makeFullPath(filename), 'utf-8');
-
 const genDiff = (filename1, filename2, formatName = 'stylish') => {
-  const data1 = readFile(filename1);
-  const data2 = readFile(filename2);
-  const parsedFile1 = parser(data1, path.extname(filename1).slice(1));
-  const parsedFile2 = parser(data2, path.extname(filename2).slice(1));
+  const fullPath1 = path.resolve(process.cwd(), filename1);
+  const fullPath2 = path.resolve(process.cwd(), filename2);
+
+  const data1 = fs.readFileSync(fullPath1, 'utf-8');
+  const data2 = fs.readFileSync(fullPath2, 'utf-8');
+
+  const parsedFile1 = parser(data1, path.extname(filename1));
+  const parsedFile2 = parser(data2, path.extname(filename2));
 
   const associationTree = buildTree(parsedFile1, parsedFile2);
   return formatters(associationTree, formatName);
