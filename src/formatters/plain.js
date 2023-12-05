e5ebf8b31f820a6f429d9a9eab70fa77cb48bfd9
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-12-05 21:35:16 +0300

    Refactoring delet iter()

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import _ from 'lodash';
 
 const makeCurrValue = (value) => {
@@ -8,29 +7,31 @@ const makeCurrValue = (value) => {
   return _.isObject(value) ? '[complex value]' : value;
 };
 
-const plain = (tree) => {
-  const iter = (node, path) => {
-    const lines = node.flatMap(({ status, key, value }) => {
-      const filePath = _.isArray(value) ? `${path}${key}.` : `${path}${key}`;
-      switch (status) {
-        case 'nested':
-          return iter(value, filePath);
-        case 'added':
-          return `Property '${filePath}' was added with value: ${makeCurrValue(value)}`;
-        case 'deleted':
-          return `Property '${filePath}' was removed`;
-        case 'changed': {
-          const { valBefore, valAfter } = value;
-          return `Property '${filePath}' was updated. From ${makeCurrValue(valBefore)} to ${makeCurrValue(valAfter)}`;
-        }
-        case 'unchanged': return [];
-        default:
-          throw new Error(`Unknown ${status}, please try again`);
+const plain = (tree, path = '') => {
+  const lines = tree.flatMap(({
+    status,
+    key,
+    value,
+    children,
+    valBefore,
+    valAfter,
+  }) => {
+    switch (status) {
+      case 'nested':
+        return plain(children, `${path}${key}.`);
+      case 'added':
+        return `Property '${path}${key}' was added with value: ${makeCurrValue(value)}`;
+      case 'deleted':
+        return `Property '${path}${key}' was removed`;
+      case 'changed': {
+        return `Property '${path}${key}' was updated. From ${makeCurrValue(valBefore)} to ${makeCurrValue(valAfter)}`;
       }
-    });
-    return lines.join('\n');
-  };
-  return `${iter(tree, '')}\n`;
+      case 'unchanged': return [];
+      default:
+        throw new Error(`Unknown ${status}, please try again`);
+    }
+  });
+  return lines.join('\n');
 };
 
 export default plain;
