b82daab9a5bb07be15638a7d1defe605ef0fd1d6
AlexeyChi <chibiryaev.alexey@yandex.ru>
2023-12-05 21:35:57 +0300

    Refactoring, added stylish()

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,58 +3,58 @@ import _ from 'lodash';
 const PARAMETRS = {
   space: ' ',
   doubleSpace: '  ',
-  minus: '- ',
-  plus: '+ ',
+  deleted: '- ',
+  added: '+ ',
   spaceCount: 4,
   leftShiftCount: 2,
 };
 
 const makeIndents = (depth) => {
-  const replacer = (PARAMETRS.spaceCount * depth) + PARAMETRS.leftShiftCount;
-  const indent = PARAMETRS.space.repeat(replacer);
-  const outIndent = PARAMETRS.space.repeat(PARAMETRS.spaceCount * depth);
-  return [indent, outIndent];
+  const newSpaceCount = PARAMETRS.spaceCount * depth - PARAMETRS.leftShiftCount;
+  return PARAMETRS.space.repeat(newSpaceCount);
+};
+
+const stringify = (node, depth) => {
+  if (!_.isObject(node)) {
+    return `${node}`;
+  }
+  const lines = Object.entries(node)
+    .map(([key, value]) => `${makeIndents(depth + 1)}${PARAMETRS.doubleSpace}${key}: ${stringify(value, depth + 1)}`)
+    .join('\n');
+
+  return `{\n${lines}\n${makeIndents(depth)}${PARAMETRS.doubleSpace}}`;
 };
 
 const stylish = (tree) => {
   const iter = (node, depth) => {
-    if (!_.isArray(node)) {
-      return _.isObject(node) ? iter(Object.entries(node), depth) : node;
-    }
-    const [indent, outIndent] = makeIndents(depth);
-
-    const lines = node.map((line) => {
-      if (_.isArray(line)) {
-        const [key, value] = line;
-        return `${indent}${PARAMETRS.doubleSpace}${key}: ${iter(value, depth + 1)}`;
-      }
-
-      const { status, key, value } = line;
-
+    const diff = node.flatMap(({
+      status,
+      key,
+      value,
+      children,
+      valBefore,
+      valAfter,
+    }) => {
       switch (status) {
-        case 'added':
-          return `${indent}${PARAMETRS.plus}${key}: ${iter(value, depth + 1)}`;
-        case 'deleted':
-          return `${indent}${PARAMETRS.minus}${key}: ${iter(value, depth + 1)}`;
-        case 'unchanged':
-          return `${indent}${PARAMETRS.doubleSpace}${key}: ${iter(value, depth + 1)}`;
         case 'nested':
-          return `${indent}${PARAMETRS.doubleSpace}${key}: ${iter(value, depth + 1)}`;
+          return `${makeIndents(depth)}${PARAMETRS.doubleSpace}${key}: {\n${iter(children, depth + 1)}\n${makeIndents(depth)}${PARAMETRS.doubleSpace}}`;
+        case 'added':
+        case 'deleted':
+          return `${makeIndents(depth)}${PARAMETRS[status]}${key}: ${stringify(value, depth)}`;
         case 'changed': {
-          const { valBefore, valAfter } = value;
-          const line1 = `${indent}${PARAMETRS.minus}${key}: ${iter(valBefore, depth + 1)}`;
-          const line2 = `${indent}${PARAMETRS.plus}${key}: ${iter(valAfter, depth + 1)}`;
+          const line1 = `${makeIndents(depth)}${PARAMETRS.deleted}${key}: ${stringify(valBefore, depth)}`;
+          const line2 = `${makeIndents(depth)}${PARAMETRS.added}${key}: ${stringify(valAfter, depth)}`;
           return `${line1}\n${line2}`;
         }
+        case 'unchanged':
+          return `${makeIndents(depth)}${PARAMETRS.doubleSpace}${key}: ${stringify(value, depth)}`;
         default:
-          throw new Error(`Unknown ${status}, please try again`);
+          throw new Error(`Unnamed ${status}`);
       }
     });
-
-    return ['{', ...lines, `${outIndent}}`].join('\n');
+    return diff.join('\n');
   };
-
-  return `${iter(tree, 0)}\n`;
+  return `{\n${iter(tree, 1)}\n}`;
 };
 
 export default stylish;
